421798be8b49ef2275ae5509df2999da3a507b15
indireshvar-venkatachalam <iv24aag@herts.ac.uk>
2025-12-11 18:32:42 +0000

    Identify the numeric columns for analaysing

--- Projectfile.R
+++ Projectfile.R
@@ -7,3 +7,9 @@ modern_renewable_energy_consumption <- readr::read_csv(
 str(modern_renewable_energy_consumption)
 
 print(colSums(is.na(modern_renewable_energy_consumption)))
+
+numeric_cols <- modern_renewable_energy_consumption %>%
+  select(where(is.numeric)) %>%
+  names()
+numeric_cols
+
